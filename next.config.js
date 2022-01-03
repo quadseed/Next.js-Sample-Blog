2228c805abda32758fef7faf7c2c3442a9b667b8
quadseed <46034259+quadseed@users.noreply.github.com>
2022-01-04 08:37:43 +0900

    Set up for GitHub Pages

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 const urlPrefix = process.env.URL_PREFIX ? '/' + process.env.URL_PREFIX : ''
 
 module.exports = {
-  reactStrictMode: true,
   assetPrefix: urlPrefix,
   basePath: urlPrefix,
   trailingSlash: true,
